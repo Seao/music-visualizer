12cef4d678a7ad041ffc164a22b758c12f33044f
Aurélien Wolz <wolzaurelien@protonmail.com>
2020-10-22 19:42:59 +0200

    Add GUI to control main options

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
-const FREQUENCIES = {
-  TOTAL: 120,
-  REDUCER: 0.7,
-  BARS: {
-    WIDTH: 5,
-    COLOR: 'rgb(228, 134, 62)'
+const gui = new dat.gui.GUI()
+
+const LOGO = {
+  SCALE: 0.4
+}
+
+const ARTIST = {
+  NAME: 'NAME',
+  FONT: {
+    SIZE: 45,
+    COLOR: '#FFF'
   }
 }
 
-const STAMP = {
-  RADIUS: 150,
-  SCALE: 0.4,
+const FREQUENCIES = {
+  TOTAL: 256,
+  SMOOTHING: 0.7,
+  BARS: {
+    WEIGHT: 2,
+    COLOR: '#FFF',
+    SPACE: 1024
+  }
 }
 
 const audioContext = new AudioContext()
 
-// background video player
+/**
+ * VIDEO
+ * - set background video player
+ */
+
 videojs('background', {
   autoplay: true,
   muted: true,
@@ -23,185 +37,218 @@ videojs('background', {
   this.play()
 })
 
+/**
+ * CANVAS
+ * - create two.js instance for 2D graphical elements
+ */
+
+const two = new Two({
+  type: Two.Types['svg'],
+  fullscreen: true
+}).appendTo(document.getElementById('canvas'))
+
+// instantiation of layers to manage overlays
+const background = two.makeGroup()
+const middleground = two.makeGroup()
+
+/**
+ * UTILS
+ */
+
+// cascade execution of synchronous functions
+const flow = (...fns) => fns.reduceRight((acc, fn) => (...x) => acc = fn(...x))
+
+// creates a mapping function that projects its input from a given range to another
+const remap = ([i0, i1], [o0, o1]) => i1 - i0 === 0
+  ? () => NaN
+  : (input) => (input - i0) / (i1 - i0) * (o1 - o0) + o0
+
+// creates a clamping function that returns the closest value to the input within the given boundaries
+const clamp = (bounds) => (input) => {
+  const [min, max] = bounds.slice().sort()
+  return Math.max(min, Math.min(input, max))
+}
+
+const isToThat = ({
+  minIs, maxIs, minThat, maxThat,
+}) => flow(
+  remap([minIs, maxIs], [minThat, maxThat]),
+  Math.round,
+  clamp([minThat, maxThat])
+)
+
+/**
+ * LOGO
+ * - makes the graphical elements composing the logo
+ * - creates utils functions to transform the logo
+ */
+
+const logo = two.makeGroup()
+
+const sprite = two.makeSprite('https://raw.githubusercontent.com/Seao/music-visualizer/development/assets/logo.png')
+sprite.scale = LOGO.SCALE
+logo.add(sprite)
+
+const artist = new Two.Text(ARTIST.NAME, 0, 0)
+artist.fill = ARTIST.FONT.COLOR
+artist.size = ARTIST.FONT.SIZE
+artist.family = 'Raleway'
+artist.weight = '600'
+logo.add(artist)
+ 
+const moveLogoToScreenCenter = () => {
+  logo.translation.set(two.width / 2, two.height / 2)
+}
+
+const setLogoScale = (value) => { sprite.scale = value }
+
+const setArtistName = (value) => { artist.value = value }
+const setArtistFontSize = (value) => { artist.size = value }
+const setArtistFontColor = (value) => { artist.fill = value }
+
+moveLogoToScreenCenter()
+
+middleground.add(logo)
+
+/**
+ * FREQUENCIES
+ * - makes the graphical elements composing the frequencies
+ * - creates utils functions to transform the frequencies
+ */
+
+const setFrequencyBarProperties = (bar = {}) => {
+  bar.linewidth = FREQUENCIES.BARS.WEIGHT
+  bar.stroke = FREQUENCIES.BARS.COLOR
+} 
+
+const frequencies = []
+for (let i = 0; i < FREQUENCIES.TOTAL; i++) {
+  // create a single frequency bar
+  const createFrequencyBar = () => {
+    const bar = two.makeLine(0, 0, 0, 0)
+    setFrequencyBarProperties(bar)
+    return bar
+  }
+
+  const frequency = two.makeGroup()
+  
+  frequency.left = createFrequencyBar()
+  frequency.add(frequency.left)
+  
+  frequency.right = createFrequencyBar()
+  frequency.add(frequency.right)
+
+  frequencies.push(frequency)
+}
+
+const translateFrequencyBar = (bar, { theta, length } = {}) => {
+  const weight = FREQUENCIES.BARS.WEIGHT
+  const space = FREQUENCIES.BARS.SPACE * (LOGO.SCALE / 2.3)
+  
+  const minX = -(space - weight) * Math.cos(theta)
+  const minY = -(space - weight) * Math.sin(theta)
+  const maxX = ((space + length) - weight) * Math.cos(theta)
+  const maxY = ((space + length) - weight) * Math.sin(theta)
+
+  setFrequencyBarProperties(bar)
+
+  const [start, finish] = bar.vertices
+  start.set((two.width / 2) + minX, (two.height / 2) + minY)
+  finish.set((two.width / 2) + maxX, (two.height / 2) + maxY)
+}
+
+const translateFrequencies = ({ level = 0, levels = [] } = {}) => {
+  const thetaLeft = isToThat({ minIs: 0, maxIs: FREQUENCIES.TOTAL - 1, minThat: (Math.PI / 2) + Math.PI, maxThat: Math.PI / 2 })
+  const thetaRight = isToThat({ minIs: 0, maxIs: FREQUENCIES.TOTAL - 1, minThat: (Math.PI / 2) + Math.PI, maxThat: (Math.PI / 2) + 2 * Math.PI })
+
+  frequencies.forEach((frequency, i) => {
+    const length = levels[i] || level
+    translateFrequencyBar(frequency.left, { theta: thetaLeft(i), length })
+    translateFrequencyBar(frequency.right, { theta: thetaRight(i), length })
+  })
+}
+
+background.add(frequencies)
+
+/**
+ * CONTROLS
+ * - intialize the GUI to control the display options of the page
+ * - set controllers reacting to user changes in options
+ */
+
+const folderLogo = gui.addFolder('Logo');
+
+const ctrlLogoScale = folderLogo.add(LOGO, 'SCALE', 0, 1.5)
+ctrlLogoScale.onChange(setLogoScale)
+
+const folderArtist = gui.addFolder('Artist');
+
+const ctrlArtistName = folderArtist.add(ARTIST, 'NAME')
+ctrlArtistName.onChange(setArtistName)
+
+const ctrlArtistFontSize = folderArtist.add(ARTIST.FONT, 'SIZE', 12, 100)
+ctrlArtistFontSize.onChange(setArtistFontSize)
+
+const folderFrequencies = gui.addFolder('Frequencies');
+
+const ctrlFrequenciesBarsColor = folderFrequencies.addColor(FREQUENCIES.BARS, 'COLOR')
+const ctrlFrequenciesBarsWeight = folderFrequencies.add(FREQUENCIES.BARS, 'WEIGHT', 1, 5, 1)
+
+
+/**
+ * SCENE
+ * - render the scene
+ */
+
+two
+  .bind('resize', () => {
+    moveLogoToScreenCenter()
+  })
+  .play()
+
+/**
+ * AUDIO PROCESSING
+ * - request access to user microphone audio
+ * - filter and analyse sound to animate graphical elements
+ */
+
 // request access to user microphone audio
 navigator.mediaDevices.getUserMedia({ audio: true })
   .then(stream => {
     const source = audioContext.createMediaStreamSource(stream) // MediaStreamSourceNode
-    const filter = audioContext.createBiquadFilter() // FilterNode
     const analyser = audioContext.createAnalyser() // AnalyserNode
     const scriptProcessor = audioContext.createScriptProcessor() // ScriptProcessorNode
 
     // set analyser configuration
-    analyser.smoothingTimeConstant = 0.5
-    analyser.fftSize = 256
-    
+    analyser.smoothingTimeConstant = FREQUENCIES.SMOOTHING
+    analyser.fftSize = FREQUENCIES.TOTAL * 2
+
+    // add gui controls
+    const ctrlFrequenciesSmoothing = folderFrequencies.add(FREQUENCIES, 'SMOOTHING', 0, 1, 0.1)
+    ctrlFrequenciesSmoothing.onChange((value) => { analyser.smoothingTimeConstant = value })
+
     // connect the audio nodes
-    source.connect(filter)
-
-    filter.connect(analyser)
-    filter.type = 'allpass'
-
+    source.connect(analyser)
     analyser.connect(scriptProcessor)
     scriptProcessor.connect(audioContext.destination)
 
-    // create two instance
-    const two = new Two({
-      type: Two.Types['svg'],
-      fullscreen: true
-    }).appendTo(document.getElementById('canvas'))
-
-    // instantiation of layers to manage overlays
-    const background = two.makeGroup()
-    const middleground = two.makeGroup()
-    const foreground = two.makeGroup()
-
-    /**
-     * Creates the elements composing the stamp.
-     */
-    const createStamp = () => {
-      const stamp = two.makeGroup()
-
-      // main stamp shape
-      const core = two.makeSprite('https://raw.githubusercontent.com/Seao/music-visualizer/development/assets/logo.png')
-      core.scale = 0.4
-
-      stamp.core = core
-      stamp.add(core)
-
-      return stamp
-    }
-
-    /**
-     * Affects a translational movement on the stamp to be centered on the screen.
-     */
-    const translateStamp = (stamp, scale = STAMP.SCALE) => {
-      stamp.translation.set(two.width / 2, two.height / 2)
-      stamp.core.scale = scale
-    }
-
-    // cascade execution of synchronous functions
-    const flow = (...fns) => fns.reduceRight((acc, fn) => (...x) => acc = fn(...x))
-
-    // Creates a mapping function that projects its input from a given range to another
-    const remap = ([i0, i1], [o0, o1]) => i1 - i0 === 0
-      ? () => NaN
-      : (input) => (input - i0) / (i1 - i0) * (o1 - o0) + o0
-
-    // Creates a clamping function that returns the closest value to the input within the given boundaries
-    const clamp = (bounds) => (input) => {
-      const [min, max] = bounds.slice().sort()
-      return Math.max(min, Math.min(input, max))
-    }
-
-    const levelToScale = ({
-      minLevel, maxLevel, minScale, maxScale,
-    }) => flow(
-      remap([minLevel, maxLevel], [minScale, maxScale]),
-      Math.round,
-      clamp([minScale, maxScale])
-    )
-
-    /**
-     * Creates the elements allowing to visualize frenquencies from audio input.
-     */
-    const createFrequencies = () => {
-      const frequencies = []
-      for (let i = 0; i < FREQUENCIES.TOTAL; i++) {
-        // create a single frequency line
-        const createFrequencyLine = () => {
-          const line = two.makeLine(0, 0, 0, 0)
-          line.linewidth = FREQUENCIES.BARS.WIDTH
-          line.stroke = FREQUENCIES.BARS.COLOR
-          return line
-        }
-
-        const frequency = two.makeGroup()
-        frequency.barLeft = createFrequencyLine()
-        frequency.barRight = createFrequencyLine()
-
-        frequency.add(frequency.barLeft)
-        frequency.add(frequency.barRight)
-
-        frequencies.push(frequency)
-      }
-      return frequencies
-    }
-
-    /**
-     * Affects a translational movement on the frequency points of the array. This method is
-     * used to adjust the levels of each point according to the analyze of the audio stream.
-     */
-    const translateFrequencies = (frequencies, { level = 0, levels = [] } = {}) => {
-      // translate the line drawed for a frquency bar
-      const translateFrequencyBar = (line, { theta, radiusMin, radiusMax } = {}) => {
-        const weight = FREQUENCIES.BARS.WIDTH
-        
-        const minX = (radiusMin - weight) * Math.cos(theta)
-        const minY = (radiusMin - weight) * Math.sin(theta)
-        
-        const maxX = (radiusMax - weight) * Math.cos(theta)
-        const maxY = (radiusMax - weight) * Math.sin(theta)
-
-        const [start, finish] = line.vertices
-        start.set((two.width / 2) + minX, (two.height / 2) + minY)
-        finish.set((two.width / 2) + maxX, (two.height / 2) + maxY)
-      }
-      
-      frequencies.forEach((frequency, i) => {
-        const theta = (pct) => (pct * Math.PI * 2) + (Math.PI + (Math.PI / 2))
-        const radiusMin = STAMP.RADIUS
-        const radiusMax = radiusMin + (levels[i] || level)
-        
-        const pctLeft = ((FREQUENCIES.TOTAL - ((i/2) + 1))) / FREQUENCIES.TOTAL
-        translateFrequencyBar(frequency.barLeft, { radiusMin, radiusMax, theta: theta(pctLeft) })
-
-        const pctRight = ((i/2) + 1) / FREQUENCIES.TOTAL
-        translateFrequencyBar(frequency.barRight, { radiusMin, radiusMax, theta: theta(pctRight) })
-      })
-    }
-
-    // create and translate stamp
-    const stamp = createStamp()
-    translateStamp(stamp)
-
-    // create and translate frequencies
-    const frequencies = createFrequencies()
-    translateFrequencies(frequencies)
-
-    // add elements in layers
-    background.add(frequencies)
-    middleground.add(stamp)
-
     // event handler to process audio input
-    scriptProcessor.onaudioprocess = (audioProcessingEvent) => {
+    scriptProcessor.onaudioprocess = () => {
       // create a new Uint8Array to store the analyser's frequencyBinCount 
       const data = new Uint8Array(analyser.frequencyBinCount)
 
       // get the byte frequency data from our array
       analyser.getByteFrequencyData(data)
 
-      const { levels, total } = data
+      const { levels, sum } = data
         .reduce((acc, level) => {
-          const levelReduced = Math.pow(level * FREQUENCIES.REDUCER, 1.1)
-          acc.levels.push(levelReduced)
-          acc.total += levelReduced
+          acc.levels.push(level)
+          acc.sum += level
           return acc
-        }, { levels: [], total: 0 })
+        }, { levels: [], sum: 0 })
 
-      const rate = levelToScale({ minLevel: 0, maxLevel: 18000, minScale: 0.3, maxScale: 0.55 })(total)
-
-      translateFrequencies(frequencies, { levels })
-      translateStamp(stamp, rate)
+      translateFrequencies({ levels })
     }
-
-    // render the scene
-    two
-      .bind('resize', () => {
-        translateStamp(stamp)
-        translateFrequencies(frequencies)
-      })
-      .play()
-
-  }, error => {
+  }, (error) => {
     // something went wrong, or the browser does not support getUserMedia
   })
